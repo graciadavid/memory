a72e2e9a88f0526211d64b97e2bc6641dfe66599
graciadavid <gracia.david@gmail.com>
2026-05-25 01:15:03 +0200

    fix: remove all RequireProfile popups, login only at game end

--- components/GameProfileGuard.tsx
+++ components/GameProfileGuard.tsx
@@ -1,21 +1,6 @@
 'use client'
 import { usePathname } from 'next/navigation'
-import RequireProfile from './RequireProfile'
-
-const GAME_PATHS = [
-  '/digits', '/sequence', '/flags', '/precision',
-  '/ace', '/geoshape', '/sudoku', '/wordle', '/wordly',
-  '/mastermind', '/2048', '/nback', '/versus',
-  '/stop', '/f1', '/pendulum', '/letter-rain', '/capitals',
-  '/blink', '/blackjack', '/profile',
-]
-
-const EXCLUDED_PATHS = ['/g/', '/play']
 
 export default function GameProfileGuard({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
-  const isExcluded = EXCLUDED_PATHS.some(p => pathname.startsWith(p))
-  const isGame = !isExcluded && GAME_PATHS.some(p => pathname.startsWith(p))
-  if (isGame) return <RequireProfile>{children}</RequireProfile>
   return <>{children}</>
 }
