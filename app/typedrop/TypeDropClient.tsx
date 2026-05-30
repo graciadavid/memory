8803d129222c00c9dfacc49ff4a34e02f93a9e7f
graciadavid <gracia.david@gmail.com>
2026-05-30 08:20:52 +0200

    fix: TypeDrop keyboard backspace at end

--- app/typedrop/TypeDropClient.tsx
+++ app/typedrop/TypeDropClient.tsx
@@ -9,7 +9,7 @@ const GOLD = '#C8960C'
 const KEYBOARD = [
   ['Q','W','E','R','T','Y','U','I','O','P'],
   ['A','S','D','F','G','H','J','K','L'],
-  ['⌫','Z','X','C','V','B','N','M','↵'],
+  ['Z','X','C','V','B','N','M','⌫'],
 ]
 
 const WORDS_BY_LEVEL = [
