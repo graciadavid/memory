6292b3929553ff3432f09125a10e02ac5b7a14c6
graciadavid <gracia.david@gmail.com>
2026-06-02 13:36:02 +0200

    add Google Ads AW tag to analytics

--- app/analytics.tsx
+++ app/analytics.tsx
@@ -2,20 +2,27 @@
 import Script from 'next/script'
 
 export default function GAAnalytics() {
- return (
-   <>
-     <Script
-       src="https://www.googletagmanager.com/gtag/js?id=G-CMRL4DM9JP"
-       strategy="afterInteractive"
-     />
-     <Script id="google-analytics" strategy="afterInteractive">
-       {`
-         window.dataLayer = window.dataLayer || [];
-         function gtag(){dataLayer.push(arguments);}
-         gtag('js', new Date());
-         gtag('config', 'G-CMRL4DM9JP');
-       `}
-     </Script>
-   </>
- )
+  return (
+    <>
+      {/* Google Analytics GA4 */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-CMRL4DM9JP"
+        strategy="afterInteractive"
+      />
+      {/* Google Ads */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17906499710"
+        strategy="afterInteractive"
+      />
+      <Script id="google-tags" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-CMRL4DM9JP');
+          gtag('config', 'AW-17906499710');
+        `}
+      </Script>
+    </>
+  )
 }
