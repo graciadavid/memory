0fbfedfb26e6560500544b4b51c9ed73e9e37497
graciadavid <gracia.david@gmail.com>
2026-06-05 01:38:32 +0200

    restore header and bottomnav in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Nunito } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import BottomNav from '@/components/BottomNav'
-import HeaderNavWrapper from '@/components/HeaderNavWrapper'
 import GAAnalytics from './analytics'
 import { Analytics as VercelAnalytics } from '@vercel/analytics/next'
 
@@ -21,8 +20,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div style={{ maxWidth: 430, margin: '0 auto', minHeight: '100dvh', position: 'relative' }}>
           <GAAnalytics />
           <VercelAnalytics />
-          <HeaderNavWrapper />
+          <Header />
           {children}
+          <BottomNav />
         </div>
       </body>
     </html>
