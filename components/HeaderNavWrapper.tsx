f2e66a322cf829416567ae59e76fa346e7858d74
graciadavid <gracia.david@gmail.com>
2026-06-05 01:33:43 +0200

    hide header and bottomnav on exactly5 route

---
+++ components/HeaderNavWrapper.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { usePathname } from 'next/navigation'
+import Header from './Header'
+import BottomNav from './BottomNav'
+
+export default function HeaderNavWrapper() {
+  const pathname = usePathname()
+  const isExactly5 = pathname?.startsWith('/exactly5')
+  if (isExactly5) return null
+  return (
+    <>
+      <Header />
+      <BottomNav />
+    </>
+  )
+}
